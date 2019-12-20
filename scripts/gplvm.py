d043e3dcdb7fd7ddb7cdbb19fdc2c5dcfd9fed46
OleguerCanal <oleguer.canal@hotmail.com>
2019-12-21 00:29:21 +0100

    IMplemented gplvm (untested)

--- scripts/gplvm.py
+++ scripts/gplvm.py
@@ -1,37 +1,96 @@
 import numpy as np
 from matplotlib import pyplot as plt
-
+from scipy.optimize import fmin_cg  # Non-linear SCG
+from scipy.stats import multivariate_normal
 from sklearn.decomposition import PCA  # For X initialization
 from sklearn.preprocessing import StandardScaler  # To standardize data
-
+from sklearn.gaussian_process import kernels
 from fake_dataset import generate_observations, plot
 np.seed = 0
 
-def fake_ivm(data, size):
-    ''' Returns random subset of the data
-        TODO(oleguer): Call real ivm algorithm by Federico
-    ''' 
-    idx = np.random.randint(data.shape[0], size=size)
-    return data[idx, :]
 
-def gplvm(data, active_set_size, iterations, latent_dimension = 2):
+def fake_ivm(data, size):
+    ''' Returns random partition of data
+        TODO(oleguer): Call real ivm algorithm by Federico
+    '''
+    indices = np.random.permutation(data.shape[0])
+    active_set, inactive_set = indices[:size], indices[size:]
+    return active_set, inactive_set
+
+
+def kernel(X, Y, alpha, beta, gamma):
+    kernel = np.matrix(kernels.RBF(length_scale=(1./gamma**2)))
+    return alpha*kernel(X, Y) + np.eye(X.shape[0])/beta
+
+
+def active_set_likelihood(params, args):
+    ''' Kernel Optimization: Equation (4) of the paper
+    '''
+    print(args)
+    Yi = args
+    print(Yi.shape)
+    Kii = kernel(Yi, Yi, alpha=params[0], beta=params[1], gamma=params[2])
+    print(Kii.shape)
+    return np.exp(-0.5*Yi.T*Kii.I*Yi) /\
+        (((2*np.pi)**(Yi.shape[1]/2.))*np.linalg.det(Kii)**0.5)
+
+
+def latent_var_prob(xj, *args):
+    ''' Latent Var Optimization: Equation (3) of the paper
+    '''
+    y_j, f_j, sigma_second_term, alpha, beta, gamma = args
+    sigma_sq_j = kernel(xj, xj, alpha, beta, gamma) - sigma_second_term
+    return multivariate_normal(f_j, sigma_sq_j).pdf(y_j)
+
+
+def gplvm(Y, active_set_size, iterations, latent_dimension=2):
+    ''' Implementation of GPLVM algorithm, returns data in latent space
+    '''
     # Initialize X through PCA
-    x = PCA(n_components = latent_dimension).fit_transform(data)
+    X = PCA(n_components=latent_dimension).fit_transform(Y)
+
     for t in range(iterations):
-        active_data = fake_ivm(data, active_set_size)
-        # return active_data
-    return x
-    
+        # Select a new active set using the IVM algorithm
+        active_set, _ = fake_ivm(Y, active_set_size)
+        Yi = np.matrix(Y[active_set, :])
+
+        # Optimise (4) wrt the parameters of K using SCG
+        kernel_params_0 = np.ones(3)  # (alpha, beta, gamma)
+        optimal_kernel_params = fmin_cg(
+            active_set_likelihood, kernel_params_0, args=Yi)
+        alpha = optimal_kernel_params[0]
+        beta = optimal_kernel_params[1]
+        gamma = optimal_kernel_params[2]
+
+        # Select a new active set
+        active_set, inactive_set = fake_ivm(Y, active_set_size)
+        Yi = np.matrix(Y[active_set, :])
+        Yj = np.matrix(Y[inactive_set, :])
+
+        Kii = kernel(Yi, Yi, alpha, beta, gamma)
+        Kii_inv = Kii.I
+        YT_Kii_inv = Yj.T*Kii_inv
+        for j in inactive_set:
+            # Optimise (3) wrt xj using SCG
+            y_j = Y[j, :]
+            k_j = Kii[:, j]
+            f_j = YT_Kii_inv*k_j
+            sigma_second_term = k_j.T*Kii_inv*k_j
+            args = tuple(y_j, f_j, sigma_second_term, alpha, beta, gamma)
+            X[j, :] = fmin_cg(latent_var_prob, X[j, :], args=args)
+    return X
+
 
 if __name__ == "__main__":
-    N = 100  # Number of observations
+    N = 500  # Number of observations
     n_classes = 3  # Number of classes
     D = 5  # Y dimension (observations)
-    
+
     observations, labels = generate_observations(N, D, n_classes)
-    # Standardize?
-    # x = StandardScaler().fit_transform(x)
-    
-    ppl_comp = gplvm(data = observations, active_set_size = 10, iterations = 1)
+    # x = StandardScaler().fit_transform(x)  # Standardize??
+
+    ppl_comp = gplvm(Y=observations,
+                     active_set_size=100,
+                     iterations=15)
 
     plot(ppl_comp, labels)
