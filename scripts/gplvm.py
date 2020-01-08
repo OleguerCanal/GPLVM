5dc86254f975a7efc9d9dc4a5b3a750a56f5b786
OleguerCanal <oleguer.canal@hotmail.com>
2020-01-08 16:24:17 +0100

    Fixed some errors in gplvm

--- scripts/gplvm.py
+++ scripts/gplvm.py
@@ -6,9 +6,10 @@ from scipy.stats import multivariate_normal
 from sklearn.decomposition import PCA  # For X initialization
 from sklearn.preprocessing import StandardScaler  # To standardize data
 from sklearn.gaussian_process import kernels
+import time
+from tqdm import tqdm
 from fake_dataset import generate_observations, plot
 
-import time
 np.seed = 0
 
 def fake_ivm(data, size):
@@ -30,8 +31,8 @@ def kernel_test(x, y, alpha, beta, gamma):
 def active_set_likelihood(params, *args):
     ''' Kernel Optimization: Equation (4) of the paper
     '''
-    Yi, YiYiT = args
-    Kii = kernel(Yi, Yi, alpha=params[0], beta=params[1], gamma=params[2])
+    Xi, YiYiT = args
+    Kii = kernel(Xi, Xi, alpha=params[0], beta=params[1], gamma=params[2])
     neg_loglike = 0.5*np.sum(inner1d(Kii.I, YiYiT)) +\
                  np.log(np.abs(np.linalg.det(Kii)))/2.
                 # + np.log(2*np.pi)*Yi.shape[1]/2. # OBS: Does not change optimization
@@ -41,13 +42,19 @@ def active_set_likelihood(params, *args):
 def latent_var_prob(xj, *args):
     ''' Latent Var Optimization: Equation (3) of the paper
     '''
-    print(np.mat(xj))
-    y_j, f_j, sigma_second_term, alpha, beta, gamma = args
-    sigma_sq_j = kernel(np.mat(xj), np.mat(xj), alpha, beta, gamma).item()
-    print(sigma_sq_j)
-    print(kernel_test(xj, xj, alpha, beta, gamma))
-    sigma_sq_j -= sigma_second_term.item()
-    cov = sigma_sq_j*np.eye(f_j.shape[0])
+    y_j, Yi, Kii_inv, k_x_x, X, alpha, beta, gamma, active_set, j = args
+    X[j, :] = xj
+    K = kernel(X, X, alpha, beta, gamma)
+    k_j = K[active_set, j]
+    
+    # Mean
+    f_j = Yi.T*Kii_inv*k_j  # TODO(oleguer): Review this, paper says Y.t but doesnt make sense
+    f_j = np.array(f_j[:, 0].flatten())[0]
+
+    # Variance
+    sigma_sq_j = k_x_x - (k_j.T*Kii_inv*k_j).item()
+    cov = sigma_sq_j*np.eye(Yi.shape[1])
+    
     # print(sigma_sq_j)
     # f_j = np.array(f_j[:, 0].flatten())[0]
     return -multivariate_normal(list(f_j), cov).logpdf(y_j)
@@ -58,39 +65,35 @@ def gplvm(Y, active_set_size, iterations, latent_dimension=2):
     '''
     # Initialize X through PCA
     X = PCA(n_components=latent_dimension).fit_transform(Y)
-    kernel_params = np.ones(3)  # (alpha, beta, gamma) TODO(oleguer): Should we rewrite those at each iteration?
+    kernel_params = np.ones(3)  # (alpha, beta, gamma) TODO(oleguer): Should we rewrite those at each iteration? I dont thinkn so
 
-    for t in range(iterations):
+    for t in tqdm(range(iterations)):
         # Select a new active set using the IVM algorithm
-        active_set, _ = fake_ivm(Y, active_set_size)
+        active_set, _ = fake_ivm(X, active_set_size)  # TODO(oleguer): Pass Y or X here?
+        Xi = np.matrix(X[active_set, :])
         Yi = np.matrix(Y[active_set, :])
         YiYiT = Yi*Yi.T  # Precompute this product
 
         # Optimise (4) wrt the parameters of K using SCG
         kernel_params = fmin_cg(
-            f = active_set_likelihood, x0 = kernel_params, args=tuple((Yi,YiYiT)))
+            f = active_set_likelihood, x0 = kernel_params, args=tuple((Xi, YiYiT)), disp=False)
         alpha = kernel_params[0]
         beta = kernel_params[1]
         gamma = kernel_params[2]
-        print(kernel_params)
+        # print("kernel_params:", kernel_params)
 
         # Select a new active set
-        active_set, inactive_set = fake_ivm(Y, active_set_size)
+        active_set, inactive_set = fake_ivm(X, active_set_size)  # TODO(oleguer): Pass Y or X here?
+        Xi = np.matrix(X[active_set, :])
         Yi = np.matrix(Y[active_set, :])
-        # Yj = np.matrix(Y[inactive_set, :])
-        Kii = kernel(Yi, Yi, alpha, beta, gamma)
-        Kii_inv = Kii.I
-        K = kernel(Y, Y, alpha, beta, gamma)
+        Kii_inv = kernel(Xi, Xi, alpha, beta, gamma).I
+        k_x_x = kernel(np.mat(np.zeros(latent_dimension)), np.mat(np.zeros(latent_dimension)), alpha, beta, gamma).item()
         for j in inactive_set:
             # Optimise (3) wrt xj using SCG
             y_j = Y[j, :]
-            k_j = K[active_set, j]
-            f_j = Yi.T*Kii_inv*k_j  # TODO(oleguer): Review this, paper says Y.t but doesnt make sense
-            f_j = np.array(f_j[:, 0].flatten())[0]
-            sigma_second_term = k_j.T*Kii_inv*k_j
-            args = tuple((y_j, f_j, sigma_second_term, alpha, beta, gamma))
-            X[j, :] = fmin_cg(latent_var_prob, X[j, :], args=args, epsilon = 0.1, disp=True)
-            quit()
+            args = tuple((y_j, Yi, Kii_inv, k_x_x, X, alpha, beta, gamma, active_set, j))
+            X[j, :] = fmin_cg(latent_var_prob, X[j, :], args=args, epsilon = 0.1, disp=False)
+            # quit()
     return X
 
 
@@ -107,5 +110,7 @@ if __name__ == "__main__":
                      iterations=10)
     pca = PCA(n_components=2).fit_transform(observations)
 
+    print("ok")
 
     plot(pca, gp_vals, labels)
+    print("ok")
