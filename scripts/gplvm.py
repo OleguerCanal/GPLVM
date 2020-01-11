0e6eed16a4440f74fc4e791fb09d1ada385396b5
Federico <federico.taschin@studenti.unipd.it>
2020-01-11 23:56:21 +0100

    Optimize active set

--- scripts/gplvm.py
+++ scripts/gplvm.py
@@ -9,26 +9,23 @@ from sklearn.gaussian_process import kernels
 import time
 from tqdm import tqdm
 from fake_dataset import generate_observations, plot
+from ivm import get_active_set
 
-np.seed = 0
-
-def fake_ivm(data, size):
-    ''' Returns random partition of data
-        TODO(oleguer): Call real ivm algorithm by Federico
-    '''
-    indices = np.random.permutation(data.shape[0])
-    active_set, inactive_set = indices[:size], indices[size:]
-    return active_set, inactive_set
+np.random.seed(1287123)
 
 
-def kernel(X, Y, alpha, beta, gamma):
+def kernel(X, Y, alpha, beta, gamma, noise=True):
     kernel = kernels.RBF(length_scale=(1./gamma**2))
-    return np.matrix(alpha*kernel(X, Y) + np.eye(X.shape[0])/beta)
+    K = np.matrix(alpha*kernel(X, Y))
+    if noise:
+        K += np.eye(X.shape[0])/beta**2
+    return K
+    
 
 def kernel_test(x, y, alpha, beta, gamma):
     return alpha*np.exp(-gamma*np.dot(x-y, x-y)/2) + 1./beta
 
-def active_set_likelihood(params, *args):
+def active_set_likelihood_params(params, *args):
     ''' Kernel Optimization: Equation (4) of the paper
     '''
     Xi, YiYiT = args
@@ -36,6 +33,13 @@ def active_set_likelihood(params, *args):
     neg_loglike = 0.5*np.sum(inner1d(Kii.I, YiYiT)) +\
                  np.log(np.abs(np.linalg.det(Kii)))/2.
                 # + np.log(2*np.pi)*Yi.shape[1]/2. # OBS: Does not change optimization
+    return neg_loglike
+
+def active_set_likelihood(X, *args):
+    YiYiT, alpha, beta, gamma, X_shape = args
+    X = X.reshape(X_shape)
+    K = kernel(X, X, alpha, beta, gamma)
+    neg_loglike = 0.5 * np.sum(inner1d(K.I, YiYiT)) + np.log(np.abs(np.linalg.det(K))) / 2
     return neg_loglike
 
 
@@ -68,33 +72,49 @@ def gplvm(Y, active_set_size, iterations, latent_dimension=2):
     kernel_params = np.ones(3)  # (alpha, beta, gamma) TODO(oleguer): Should we rewrite those at each iteration? I dont thinkn so
 
     for t in tqdm(range(iterations)):
+        cur_kernel = kernel(X, X, *kernel_params, noise=False)
         # Select a new active set using the IVM algorithm
-        active_set, _ = fake_ivm(X, active_set_size)  # TODO(oleguer): Pass Y or X here?
+        active_set, _ = get_active_set(cur_kernel, kernel_params[1], active_set_size)
+        print(active_set)
+        
         Xi = np.matrix(X[active_set, :])
         Yi = np.matrix(Y[active_set, :])
         YiYiT = Yi*Yi.T  # Precompute this product
 
         # Optimise (4) wrt the parameters of K using SCG
         kernel_params = fmin_cg(
-            f = active_set_likelihood, x0 = kernel_params, args=tuple((Xi, YiYiT)), disp=False)
-        alpha = kernel_params[0]
-        beta = kernel_params[1]
-        gamma = kernel_params[2]
-        # print("kernel_params:", kernel_params)
+            f = active_set_likelihood_params, x0 = kernel_params, args=tuple((Xi, YiYiT)), disp=True)
+
+        # Optimize wrt XI (active set latent variables) (optional, why??)
+        Xi = optimize_active(Xi, Yi, *kernel_params)
+        for row, idx in zip(Xi, active_set):
+            X[idx] = row
 
         # Select a new active set
-        active_set, inactive_set = fake_ivm(X, active_set_size)  # TODO(oleguer): Pass Y or X here?
+        cur_kernel = kernel(X, X, *kernel_params, noise=False)
+        active_set, inactive_set = get_active_set(cur_kernel, kernel_params[1], active_set_size)
         Xi = np.matrix(X[active_set, :])
         Yi = np.matrix(Y[active_set, :])
-        Kii_inv = kernel(Xi, Xi, alpha, beta, gamma).I
-        k_x_x = kernel(np.mat(np.zeros(latent_dimension)), np.mat(np.zeros(latent_dimension)), alpha, beta, gamma).item()
+        Kii_inv = kernel(Xi, Xi, *kernel_params).I
+        k_x_x = kernel(np.mat(np.zeros(latent_dimension)), np.mat(np.zeros(latent_dimension)),
+                       *kernel_params).item()
         for j in inactive_set:
             # Optimise (3) wrt xj using SCG
             y_j = Y[j, :]
-            args = tuple((y_j, Yi, Kii_inv, k_x_x, X, alpha, beta, gamma, active_set, j))
-            X[j, :] = fmin_cg(latent_var_prob, X[j, :], args=args, epsilon = 0.1, disp=False)
-            # quit()
+            args = tuple((y_j, Yi, Kii_inv, k_x_x, X, *kernel_params, active_set, j))
+            X[j, :] = fmin_cg(latent_var_prob, X[j, :], args=args, disp=False)
     return X
+
+def optimize_active(X, Y, alpha, beta, gamma):
+    assert X.shape[0] == Y.shape[0]
+    Y = np.matrix(Y)
+    latent_dimension = X.shape[1]
+    X_0 = np.random.normal(0, 1, X.shape).flatten()
+    YiYiT = Y * Y.T
+    args = tuple((YiYiT, alpha, beta, gamma, X.shape))
+    X_opt = fmin_cg(f = active_set_likelihood, x0 = X_0, args=args)
+    return X_opt
+
 
 
 if __name__ == "__main__":
